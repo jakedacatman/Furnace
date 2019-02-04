e8d10e088ca5e40c774191db86e670acd12a9f92
jakedacatman <32282272+jakedacatman@users.noreply.github.com>
2019-02-04 17:02:03 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 {
-  inputChest = "" -- Put the network name of the storage item you want to use as an input.
-  outputChest = "" -- Put the network name of the storage item you want to use as an output.
+  inputChest = "", -- Put the network name of the storage item you want to use as an input.
+  outputChest = "", -- Put the network name of the storage item you want to use as an output.
 }
