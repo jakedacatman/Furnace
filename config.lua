989e9dcbdf844a1187974f1bb38a44e52eb31f03
Jacob <32282272+jakedacatman@users.noreply.github.com>
2020-01-24 18:51:29 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 {
   inputChest = "", -- Put the network name of the storage item you want to use as an input.
   outputChest = "", -- Put the network name of the storage item you want to use as an output.
+  fuel = false, -- Put the full name of the fuel you intend to use, as a string. eg. fuel = "minecraft:coal"
 }
